e436b363b645fba850bf630e6dc44f47b4924e9e
akorede ridwanullahi abiodun <akoredera@gmail.com>
2024-08-14 12:16:21 -0700

    modified task 9 files

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -8,4 +8,4 @@ def insert_school(mongo_collection, **kwargs):
     '''
     a Python function that inserts a new document in a collection based on kwargs
     '''
-    return mongo_collection.insert_one(kwargs)
+    return mongo_collection.insert(kwargs)
