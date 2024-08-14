1a49e4d3f0206c22f2cf0bb025a932f8cbe76864
akorede ridwanullahi abiodun <akoredera@gmail.com>
2024-08-14 13:04:58 -0700

    add task 10 files

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,4 +5,7 @@
 
 
 def update_topics(mongo_collection, name, topics):
-    mongo_collection.update_one({'name': name}, {"$set" : {'topics': topics}})
+    '''
+    Python function that changes all topics of a school document based on the name
+    '''
+    mongo_collection.update({'name': name}, {'$set' : {'topics': topics}})
