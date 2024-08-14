0d3c0f2458fa6c46f98754b57ab21601b6fd6cf2
akorede ridwanullahi abiodun <akoredera@gmail.com>
2024-08-14 13:55:12 -0700

    add task 12 file

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''Python script that provides some stats about Nginx logs stored in MongoDB'''
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db_collection = client.logs.nginx
+    n_logs = db_collection.count_documents({})
+    print(f'{n_logs} logs')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        count = db_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+
+    status_check = db_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f'{status_check} status check')
